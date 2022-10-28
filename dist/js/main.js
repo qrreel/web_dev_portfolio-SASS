8906b23b4d5669df385c4f04bcc5d8c7195b0e8e
Rafał <rafalwas88@gmail.com>
2022-10-28 18:56:15 +0200

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,18 @@
+const menuBtn = document.querySelector(".menu-btn");
+const burger = document.querySelector(".menu-btn__burger");
+const nav = document.querySelector(".nav");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".menu-nav__item");
+
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenuButton);
+
+function toggleMenuButton() {
+  burger.classList.toggle("open");
+  nav.classList.toggle("open");
+  menuNav.classList.toggle("open");
+  navItems.forEach((item) => item.classList.toggle("open"));
+
+  showMenu = !showMenu;
+}
